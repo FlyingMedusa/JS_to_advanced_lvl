f5ee8fa8f65f9da7088e1385562035d112f9f97a
FlyingMedusa <sleboda.m98@gmail.com>
2022-03-21 20:16:57 +0100

    basics

---
+++ 001_console.js
@@ -0,0 +1,6 @@
+console.log("Hello, World");
+console.log(Math.round(3.14));
+console.log(Math.floor(3.14));
+console.log(Math.ceil(3.14));
+
+window.alert(5 + 5);
---
+++ 002_external_js.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("main_title").innerHTML = "The title has changed";
+  }
